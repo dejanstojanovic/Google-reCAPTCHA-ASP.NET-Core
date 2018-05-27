a59abc5db1ab247ca16d0eb35b1f151de2941e8f
dejanstojanovic <d.stojanovic@hotmail.com>
2018-05-27 12:14:09 +0400

    Add HtmlHelper

---
+++ Sample.Core.GoogleReCaptcha.Web/Validation/GoogleReCaptchaTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.IO;
+
+namespace Sample.Core.GoogleReCaptcha.Web.Validation
+{
+    public static class GoogleReCaptchaTagHelper
+    {
+        public static IHtmlContent GoogleReCaptcha(this IHtmlHelper htmlHelper, String siteKey, String callback = null)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.Attributes.Add("class", "g-recaptcha");
+            tagBuilder.Attributes.Add("data-sitekey", siteKey);
+            if (callback != null && String.IsNullOrWhiteSpace(callback))
+            {
+                tagBuilder.Attributes.Add("data-callback", callback);
+            }
+            using (var writer = new StringWriter())
+            {
+                tagBuilder.WriteTo(writer, System.Text.Encodings.Web.HtmlEncoder.Default);
+                var htmlOutput = writer.ToString();
+                return htmlHelper.Raw(htmlOutput);
+            }
+        }
+    }
+}
