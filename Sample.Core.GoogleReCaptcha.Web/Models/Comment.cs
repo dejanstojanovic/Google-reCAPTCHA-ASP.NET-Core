a7d7d817d4cf492c2b1afb5e0751d2f145197b52
dejanstojanovic <d.stojanovic@hotmail.com>
2018-05-27 09:24:09 +0400

    Using abstract abes model

--- Sample.Core.GoogleReCaptcha.Web/Models/Comment.cs
+++ Sample.Core.GoogleReCaptcha.Web/Models/Comment.cs
@@ -6,17 +6,12 @@ using Newtonsoft.Json;
 
 namespace Sample.Core.GoogleReCaptcha.Web.Models
 {
-    public class Comment
+    public class Comment : GoogleReCaptchaModelBase
     {
         [Required]
         public String Title { get; set; }
 
         [Required]
         public String Content { get; set; }
-
-        [Required]
-        [GoogleReCaptchaValidation]
-        [BindProperty(Name = "g-recaptcha-response")]
-        public String GoogleReCaptchaResponse { get; set; }
     }
 }
---
+++ Sample.Core.GoogleReCaptcha.Web/Validation/GoogleReCaptchaModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample.Core.GoogleReCaptcha.Web.Validation
+{
+    public abstract class GoogleReCaptchaModelBase
+    {
+        [Required]
+        [GoogleReCaptchaValidation]
+        [BindProperty(Name = "g-recaptcha-response")]
+        public String GoogleReCaptchaResponse { get; set; }
+    }
+}
