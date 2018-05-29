2fa896f208a8d249fe3882344d9108faf9991005
dejanstojanovic <d.stojanovic@hotmail.com>
2018-05-29 13:46:27 +0400

    Invisible reCAPTCHA

--- Sample.Core.GoogleReCaptcha.Web/Validation/GoogleReCaptchaTagHelper.cs
+++ Sample.Core.GoogleReCaptcha.Web/Validation/GoogleReCaptchaTagHelper.cs
@@ -9,13 +9,33 @@ namespace Sample.Core.GoogleReCaptcha.Web.Validation
     {
         public static IHtmlContent GoogleReCaptcha(this IHtmlHelper htmlHelper, String siteKey, String callback = null)
         {
-            var tagBuilder = new TagBuilder("div");
+            var tagBuilder = GetReCaptchaTag("div", siteKey, callback);
+            return GetHtmlContent(htmlHelper, tagBuilder);
+        }
+
+
+        public static IHtmlContent GoogleInvisibleReCaptcha(this IHtmlHelper htmlHelper, String text,  String siteKey, String callback=null)
+        {
+            var tagBuilder = GetReCaptchaTag("button", siteKey, callback);
+            tagBuilder.InnerHtml.Append(text);
+            return GetHtmlContent(htmlHelper, tagBuilder);
+        }
+
+        private static TagBuilder GetReCaptchaTag(String tagName, String siteKey, String callback = null)
+        {
+            var tagBuilder = new TagBuilder(tagName);
             tagBuilder.Attributes.Add("class", "g-recaptcha");
             tagBuilder.Attributes.Add("data-sitekey", siteKey);
             if (callback != null && String.IsNullOrWhiteSpace(callback))
             {
                 tagBuilder.Attributes.Add("data-callback", callback);
             }
+
+            return tagBuilder;
+        }
+
+        private static IHtmlContent GetHtmlContent(IHtmlHelper htmlHelper,TagBuilder tagBuilder)
+        {
             using (var writer = new StringWriter())
             {
                 tagBuilder.WriteTo(writer, System.Text.Encodings.Web.HtmlEncoder.Default);
@@ -23,5 +43,7 @@ namespace Sample.Core.GoogleReCaptcha.Web.Validation
                 return htmlHelper.Raw(htmlOutput);
             }
         }
+
+
     }
 }
